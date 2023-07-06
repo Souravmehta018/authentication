dd5125b74de73fa8dadcc4e98296bbccbce45e89
Sourav <mehtas018@gmail.com>
2023-07-07 01:53:33 +0530

    abcde

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,11 @@
+'use client'      //to make this page a client component
+import Link from "next/link"
+import React from "react"
+import { useRouter } from "next/navigation"
+import axios from "axios"
+
+
+
 function signUp() {
   return (
     <div>

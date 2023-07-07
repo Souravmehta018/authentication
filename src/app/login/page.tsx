88c0fb37caf076f807778c662bf1370e7d60cad9
Sourav <mehtas018@gmail.com>
2023-07-07 16:21:51 +0530

    linked 2 pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
+'use client'      //to make this page a client component
+import React from "react"
+import { useRouter } from "next/navigation"
+import Link from "next/link"
+// import {axios} from "axios"
 
-function loginPage() {
+
+
+function LogIn() {
+  const [user, setUser] = React.useState({
+    email: "", password: ""
+  })
+  const onLogin =async () => {
+    
+  }
+
   return (
     <div>
-      <h1> LognIn </h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+          <h1> Log In </h1>
+          <hr />
+
+          <label htmlFor="email"> Email</label>
+          <input 
+          className="4text:black p-2 border border-gray-300 rounded-r-lg mb-4 focus:outline-none focus:border-gray-600 "
+          type="text" id="email" value={user.email} 
+          onChange={(e) => setUser({...user, email: e.target.value})} 
+          placeholder="Email"/>
+
+          <label htmlFor="password"> Password</label>
+          <input 
+          className="4text:black p-2 border border-gray-300 rounded-r-lg mb-4 focus:outline-none focus:border-gray-600 "
+          type="text" id="password" value={user.password} 
+          onChange={(e) => setUser({...user, password: e.target.value})} 
+          placeholder="password"/>
+
+          <button onClick={onLogin}
+          className="p-2 border border-gray-300 rounder-lg mb-4 focus:outline-none
+          focus:border-gray-600"> LogIn here</button>
+
+        <Link href={'/signup'} > Move to Sign Up </Link>
+        </div>
+      
     </div>
   )
 }
 
-export default loginPage
+export default LogIn
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,17 +2,54 @@
 import Link from "next/link"
 import React from "react"
 import { useRouter } from "next/navigation"
-import axios from "axios"
+// import {axios} from "axios"
 
 
 
-function signUp() {
+function SignUp() {
+  const [user, setUser] = React.useState({
+    email: "", password: "", username:""
+  })
+  const onSignup =async () => {
+    
+  }
+
   return (
     <div>
-        <h1> SignUp</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+          <h1> Sign Up </h1>
+          <hr />
+          <label htmlFor="username"> Username</label>
+          <input 
+          className="4text:black p-2 border border-gray-300 rounded-r-lg mb-4 focus:outline-none focus:border-gray-600 "
+          type="text" id="username" value={user.username} 
+          onChange={(e) => setUser({...user, username: e.target.value})} 
+          placeholder="username"
+          />
+        <div className="flex">
+          <label htmlFor="email" > Email</label>
+          <input 
+          className="4text:black p-2 border border-gray-300 rounded-r-lg mb-4 focus:outline-none focus:border-gray-600 "
+          type="text" id="email" value={user.email} 
+          onChange={(e) => setUser({...user, email: e.target.value})} 
+          placeholder="Email"/>
+        </div>
+          <label htmlFor="password"> Password</label>
+          <input 
+          className="4text:black p-2 border border-gray-300 rounded-r-lg mb-4 focus:outline-none focus:border-gray-600 "
+          type="text" id="password" value={user.password} 
+          onChange={(e) => setUser({...user, password: e.target.value})} 
+          placeholder="password"/>
+
+          <button onClick={onSignup}
+          className="p-2 border border-gray-300 rounder-lg mb-4 focus:outline-none
+          focus:border-gray-600"> SignUp</button>
+
+      <Link href={"/login"}> Move to Login </Link>
+        </div>
       
     </div>
   )
 }
 
-export default signUp
+export default SignUp

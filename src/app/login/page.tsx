7a411615ee03a7f36d6ac9e5cb32a545c035b3b1
Sourav <mehtas018@gmail.com>
2023-07-08 10:06:55 +0530

    learning shit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client'      //to make this page a client component
+import Link from "next/link"
 import React from "react"
 import { useRouter } from "next/navigation"
-import Link from "next/link"
 // import {axios} from "axios"
 
 
@@ -10,35 +10,38 @@ function LogIn() {
   const [user, setUser] = React.useState({
     email: "", password: ""
   })
-  const onLogin =async () => {
+  const onLogIn =async () => {
     
   }
 
   return (
     <div>
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-          <h1> Log In </h1>
+          <h1 className="text-3xl italic underline font-bold"> LogIn </h1>
           <hr />
-
-          <label htmlFor="email"> Email</label>
+          <br />
+        <div className="flex">
+          <label className="text-xl" htmlFor="email">Email: </label>
           <input 
           className="4text:black p-2 border border-gray-300 rounded-r-lg mb-4 focus:outline-none focus:border-gray-600 "
           type="text" id="email" value={user.email} 
           onChange={(e) => setUser({...user, email: e.target.value})} 
           placeholder="Email"/>
-
-          <label htmlFor="password"> Password</label>
+        </div>
+        <div className="flex">
+          <label className="text-xl" htmlFor="password">Password: </label>
           <input 
           className="4text:black p-2 border border-gray-300 rounded-r-lg mb-4 focus:outline-none focus:border-gray-600 "
           type="text" id="password" value={user.password} 
           onChange={(e) => setUser({...user, password: e.target.value})} 
           placeholder="password"/>
+        </div>
 
-          <button onClick={onLogin}
+          <button onClick={onLogIn}
           className="p-2 border border-gray-300 rounder-lg mb-4 focus:outline-none
-          focus:border-gray-600"> LogIn here</button>
+          focus:border-gray-600"> LogIn</button>
 
-        <Link href={'/signup'} > Move to Sign Up </Link>
+      <Link href={"/signup"}> Move to SignUp </Link>
         </div>
       
     </div>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -17,29 +17,34 @@ function SignUp() {
   return (
     <div>
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-          <h1> Sign Up </h1>
+          <h1 className="text-3xl italic underline font-bold"> Sign Up </h1>
           <hr />
-          <label htmlFor="username"> Username</label>
+          <br />
+        <div className="flex">  
+          <label className="text-xl" htmlFor="username">Username: </label>
           <input 
           className="4text:black p-2 border border-gray-300 rounded-r-lg mb-4 focus:outline-none focus:border-gray-600 "
           type="text" id="username" value={user.username} 
           onChange={(e) => setUser({...user, username: e.target.value})} 
           placeholder="username"
           />
+        </div>
         <div className="flex">
-          <label htmlFor="email" > Email</label>
+          <label className="text-xl" htmlFor="email">Email: </label>
           <input 
           className="4text:black p-2 border border-gray-300 rounded-r-lg mb-4 focus:outline-none focus:border-gray-600 "
           type="text" id="email" value={user.email} 
           onChange={(e) => setUser({...user, email: e.target.value})} 
           placeholder="Email"/>
         </div>
-          <label htmlFor="password"> Password</label>
+        <div className="flex">
+          <label className="text-xl" htmlFor="password">Password: </label>
           <input 
           className="4text:black p-2 border border-gray-300 rounded-r-lg mb-4 focus:outline-none focus:border-gray-600 "
           type="text" id="password" value={user.password} 
           onChange={(e) => setUser({...user, password: e.target.value})} 
           placeholder="password"/>
+        </div>
 
           <button onClick={onSignup}
           className="p-2 border border-gray-300 rounder-lg mb-4 focus:outline-none
